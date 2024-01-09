7c47bb99d8540d377c755f0fb1c3cf365fe507aa
KirillR1 <98412221+KirillR1@users.noreply.github.com>
2024-01-09 18:54:17 +0300

    Add files via upload

--- Ping_Pong.py
+++ Ping_Pong.py
@@ -1,12 +1,14 @@
 from pygame import *
+import os
 
-
+font.init()
 win_height = 700
 win_width = 500
 window = display.set_mode((win_height, win_width))
 display.set_caption("PingPong")
-background = transform.scale(image.load("black.jpg"), (700, 500))
-
+#background = transform.scale(image.load("ball.png"), (win_height, win_width))
+window.fill((0, 0, 0))
+font2 = font.SysFont('Arial', 50)
 
 class GameSprite(sprite.Sprite):
         def __init__(self, sprt_image, sprt_x, sprt_y, sprt_speed, width, height, key_up, key_down):
@@ -35,14 +37,18 @@ class Ball(GameSprite):
         #self.rect.x += self.speed
         pass
 
-
+current_path = os.path.dirname(__file__)
     
 
 
-player1 = Player("raketka.png", 68, 227, 5, 30, 80, K_w, K_s)
-player2 = Player("raketka.png", 600, 227, 5, 30, 80, K_UP, K_DOWN)
-ball = Ball("ball.png", 300, 250, 5, 40, 40, K_h, K_y)
+player1 = Player(os.path.join(current_path, 'raketka.png'), 68, 227, 5, 30, 80, K_w, K_s)
+player2 = Player(os.path.join(current_path, 'raketka.png'), 600, 227, 5, 30, 80, K_UP, K_DOWN)
+ball = Ball(os.path.join(current_path, 'ball.png'), 300, 250, 5, 40, 40, K_h, K_y)
 
+txt_WIN1 = font2.render("PLAYER 1 WIN", 1, (0, 255, 0))
+txt_LOSE1 = font2.render("PLAYER 1 LOSE", 1, (255, 0, 0))
+txt_WIN2 = font2.render("PLAYER 2 WIN", 1, (0, 255, 0))
+txt_LOSE2 = font2.render("PLAYER 2 LOSE", 1, (255, 0, 0))
 
 b_speed_x = 4
 b_speed_y = 4
@@ -57,7 +63,7 @@ while run:
                 run = False
 
     if finish != True:
-            window.blit(background, (0, 0))
+            window.fill((0, 0, 0))
             player1.update()
             player1.reset()
             player2.update()
@@ -69,9 +75,15 @@ while run:
                 b_speed_y *= -1
             if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
                 b_speed_x *= -1
-                #b_speed_y *= -1
-            if ball.rect.x > win_height-40 or ball.rect.x < 0:
+            if ball.rect.x > win_height-40: #or ball.rect.x < 0:
+                window.blit(txt_LOSE2, (205, 270))
+                window.blit(txt_WIN1, (220, 190))
                 finish = True
+            if ball.rect.x < 0:
+                window.blit(txt_LOSE1, (205, 270))
+                window.blit(txt_WIN2, (220, 190))
+                finish = True
+
 
     display.update()
     clock.tick(FPS)
